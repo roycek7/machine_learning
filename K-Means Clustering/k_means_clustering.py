ec22fbb91fe53d4d4a91580afe63c211ba6ef31e
roycek7 <pritishkumar.roy@uqconnect.edu.au>
2020-09-08 21:51:02 +1000

    incorporate sse into clustering

--- K-Means Clustering/k_means_clustering.py
+++ K-Means Clustering/k_means_clustering.py
@@ -12,17 +12,15 @@ def normalize_data(data):
     return (data - np.min(data)) / (np.max(data) - np.min(data))
 
 
-def check_convergence(centroid_old, centroids, K):
-    distance = [euclidean_distance(centroid_old[i], centroids[i]) for i in range(K)]
-    return sum(distance) == 0
-
-
 def calculate_centroids(data, cluster_centroid, K):
-    new_centroid = []
+    new_centroid, sse = [], 0
     for _ in range(K):
         new_centroids = [data[i] for i in range(len(data)) if cluster_centroid[i] == _]
         mean_centroid = np.mean(new_centroids, axis=0)
         new_centroid.append(mean_centroid)
+        for i in range(len(new_centroids)):
+            sse = np.sum(np.square(new_centroids[i] - new_centroid[_]))
+    print(f'SSE: {sse}')
     return new_centroid
 
 
@@ -45,16 +43,15 @@ def assign_clusters(data, centers):
     return clusters_list
 
 
-def get_variance(old_c, current_centroid, K):
+def get_convergence(old_c, current_centroid, K):
     total_sum = 0
     for _ in range(K):
         total_sum += np.linalg.norm(old_c[_] - current_centroid[_])
-    print(f'MSE: {total_sum * 100}')
     return total_sum
 
 
 clustering_data = np.genfromtxt('simple.txt')
-# clustering_data = np.delete(clustering_data, np.s_[0], axis=0)
+clustering_data = np.delete(clustering_data, np.s_[0], axis=0)
 # clustering_data = np.delete(clustering_data, np.s_[0], axis=1)
 
 K = 3
@@ -72,7 +69,7 @@ for i in range(20):
     centroid = calculate_centroids(clustering_data, new_clusters[i - 1] if n_c else initial_clusters, K)
     new_clusters.append(assign_clusters(clustering_data, centroid)), old_centroid.append(centroid)
     if n_c:
-        if get_variance(old_centroid[i - 1], centroid, K) == 0:
+        if get_convergence(old_centroid[i - 1], centroid, K) == 0:
             break
     n_c = True
 
